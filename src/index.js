b532ae31053679b51d3ebb00939b782534667bd6
tylermoroz <moroztyler@gmail.com>
2023-04-28 15:26:13 -0600

    Reset project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
-import todo from './todo.js';
-import project from './project.js';
-
-console.log(project("build chair", "build a wooden chair for the home office", "October 28, 2022", 5, [todo("legs", "build legs out of wood", "3:00 pm", 1, "building legs first as foundation for seat", false), todo("seat", "build seat out of wood", "5:00 pm", 2, "build seat out of wood and add inserts for all 4 legs and 7 posts for the backrest", false), todo("backrest", "build backrest out of wood", "7:00 pm", 3, "build backrest out of wood and a hinge to recline when the user leans back", false)]));
+console.log("check");
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,19 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-        hot: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'To-Do List',
-        }),
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
+  },
 };
