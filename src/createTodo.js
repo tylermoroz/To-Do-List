ce74452f8d3e16736bf14af6b363779d05ff5378
tylermoroz <moroztyler@gmail.com>
2023-05-16 15:35:33 -0600

    Add function to change the background color of each todo based on priority value. Add css styling for todos and todoDiv.

--- src/createTodo.js
+++ src/createTodo.js
@@ -1,7 +1,7 @@
 import { submit } from "./todoForm.js";
 import { displayTodo } from "./displayTodo.js";
 
-const todoArray = [];
+let todoArray = [];
 
 const Todo = (title, description, dueDate, priority, notes, complete) => {
   return { title, description, dueDate, priority, notes, complete };
@@ -9,6 +9,7 @@ const Todo = (title, description, dueDate, priority, notes, complete) => {
 
 const newTodo = (event) => {
   event.preventDefault();
+  todoArray = [];
   let todo = Todo();
   todo.title = document.getElementById("title").value;
   todo.description = document.getElementById("description").value;
--- src/displayTodo.js
+++ src/displayTodo.js
@@ -1,22 +1,36 @@
 import { todoArray } from "./createTodo.js";
 import { todoDiv } from "./index.js";
 
+const priorityColor = (i, todo) => {
+  if (todoArray[i].priority == "0") {
+    todo.style.backgroundColor = "#6bed7c";
+  } else if (todoArray[i].priority == "1") {
+    todo.style.backgroundColor = "#f2f074";
+  } else if (todoArray[i].priority == "2") {
+    todo.style.backgroundColor = "#f75252";
+  }
+};
+
 const displayTodo = () => {
   for (let i = 0; i < todoArray.length; i++) {
     const todoObject = document.createElement("div");
     const title = document.createElement("h3");
     const description = document.createElement("p");
     const dueDate = document.createElement("p");
-    const priority = document.createElement("p");
+    // const priority = document.createElement("p");
     const notes = document.createElement("p");
     const complete = document.createElement("p");
 
     title.textContent = todoArray[i].title;
     description.textContent = todoArray[i].description;
     dueDate.textContent = todoArray[i].dueDate;
-    priority.textContent = todoArray[i].priority;
+    // priority.textContent = todoArray[i].priority;
     notes.textContent = todoArray[i].notes;
     complete.textContent = todoArray[i].complete;
+
+    priorityColor(i, todoObject);
+
+    todoObject.classList.add("todo-object");
 
     todoDiv.appendChild(todoObject);
     todoObject.appendChild(title);
@@ -25,7 +39,7 @@ const displayTodo = () => {
     todoObject.appendChild(priority);
     todoObject.appendChild(notes);
     todoObject.appendChild(complete);
-    console.log(todoObject);
+    console.log(todoObject, todoArray[i]);
   }
 };
 
