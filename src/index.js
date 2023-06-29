540b77f88b781e2eeeb042241f0ccdd001af7721
tylermoroz <moroztyler@gmail.com>
2023-06-29 15:25:35 -0600

    Too many bugs, code was unstable and a nightmare to update. Nuked it.

--- src/index.js
+++ src/index.js
@@ -1,42 +1,5 @@
-import { formCreate, formClear, formDiv } from "./todoForm";
-import { createTodo } from "./createTodo.js";
-import {
-  activeProjectHead,
-  activeProjectContainer,
-  activeProjectTodos,
-  activeProjectTitle,
-} from "./displayTodo.js";
 import "./style.css";
-import { projectsContainer, addProjectBtn } from "./projects.js";
-import {
-  activeProject,
-  addProject,
-  displayActiveProject,
-  newProjectFormCreate,
-  selectProject,
-} from "./newProject.js";
 
 const content = document.querySelector("#content");
-
-activeProjectContainer.setAttribute("id", "active-project-container");
-activeProjectHead.setAttribute("id", "active-project-head");
-activeProjectTodos.setAttribute("id", "active-project-todos");
-activeProjectTitle.setAttribute("id", "active-project-title");
-
-content.appendChild(formDiv);
-content.appendChild(projectsContainer);
-content.appendChild(activeProjectContainer);
-activeProjectContainer.appendChild(activeProjectHead);
-activeProjectHead.appendChild(activeProjectTitle);
-activeProjectContainer.appendChild(activeProjectTodos);
-
-formCreate();
-addProject();
-createTodo();
-formClear();
-addProjectBtn.addEventListener("click", newProjectFormCreate);
-selectProject();
-activeProject();
-displayActiveProject();
 
 export { content };
