d67732300ebc55b3001bdae091c5557613be4919
tylermoroz <moroztyler@gmail.com>
2023-06-30 15:45:30 -0600

    Add functions to create projects and todos.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./style.css";
+import { projectsArray } from "./projects.js";
 
 const content = document.querySelector("#content");
+
+console.log(projectsArray);
 
 export { content };
---
+++ src/projects.js
@@ -0,0 +1,70 @@
+const projectsArray = [];
+
+const Project = (title, todos) => {
+  return { title, todos };
+};
+
+const createProject = (title, todos) => {
+  let newProject = Project();
+  newProject.title = title;
+  newProject.todos = [...todos];
+  projectsArray.push(newProject);
+};
+
+createProject("workout", []);
+createProject("cook", []);
+
+const Todo = (title, dueDate, description, priority, notes, complete) => {
+  return { title, dueDate, description, priority, notes, complete };
+};
+
+const createTodo = (
+  title,
+  dueDate,
+  description,
+  priority,
+  notes,
+  complete,
+  newProject
+) => {
+  let newTodo = Todo();
+  newTodo.title = title;
+  newTodo.dueDate = dueDate;
+  newTodo.description = description;
+  newTodo.priority = priority;
+  newTodo.notes = notes;
+  newTodo.complete = complete;
+  newProject.push(newTodo);
+};
+
+createTodo(
+  "deadlift",
+  "10-28-2023",
+  "feet shoulder width apart and lift with hips.",
+  "high",
+  "stretch, try for 3x12",
+  "incomplete",
+  projectsArray[0].todos
+);
+
+createTodo(
+  "bench",
+  "11-23-2189",
+  "elbows tucked, bring bar to chest and press up",
+  "high",
+  "warm up, try for 3x8",
+  "incomplete",
+  projectsArray[0].todos
+);
+
+createTodo(
+  "chicken",
+  "09-03-2047",
+  "marinate chicken breast, heat bbq to 350, cook chicken for 8 minutes on each side or until inside temp is 160 degrees",
+  "high",
+  "use maple marinade",
+  "incomplete",
+  projectsArray[1].todos
+);
+
+export { projectsArray };
