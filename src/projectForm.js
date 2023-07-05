6c851c6f57b0e47d8d81c014bcd682a6fddac65b
tylermoroz <moroztyler@gmail.com>
2023-07-05 15:44:34 -0600

    Adjust layout on projects div.

--- src/projectForm.js
+++ src/projectForm.js
@@ -1,22 +1,28 @@
 const projectFormDiv = document.createElement("div");
 const projectDivHead = document.createElement("div");
-const projectDivTitle = document.createElement("h3");
+const projectTitleDiv = document.createElement("div");
+const projectsTitle = document.createElement("p");
 const projectListDiv = document.createElement("div");
+const projectsBtnDiv = document.createElement("div");
 const addProjectBtn = document.createElement("button");
 
-projectDivTitle.textContent = "Projects";
+projectsTitle.textContent = "Projects";
 addProjectBtn.textContent = "+";
 
 projectFormDiv.classList.add("div-background");
 projectFormDiv.setAttribute("id", "project-form-div");
 projectDivHead.setAttribute("id", "project-div-head");
-projectDivTitle.setAttribute("id", "project-div-title");
+projectTitleDiv.setAttribute("id", "title-div");
 projectListDiv.setAttribute("id", "projects-list-div");
-addProjectBtn.setAttribute("id", "add-project-button");
+projectsBtnDiv.setAttribute("id", "button-div");
+projectsTitle.setAttribute("id", "projects-title");
+addProjectBtn.setAttribute("id", "projects-btn");
 
 projectFormDiv.appendChild(projectDivHead);
-projectDivHead.appendChild(projectDivTitle);
-projectDivHead.appendChild(addProjectBtn);
+projectDivHead.appendChild(projectTitleDiv);
+projectDivHead.appendChild(projectsBtnDiv);
+projectTitleDiv.appendChild(projectsTitle);
+projectsBtnDiv.appendChild(addProjectBtn);
 projectFormDiv.appendChild(projectListDiv);
 
 export { projectFormDiv };
